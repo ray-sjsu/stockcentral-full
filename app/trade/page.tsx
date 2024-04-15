360ea0ba1174a26153628098492554227422679e
ray-sjsu <raymund.mercader@sjsu.edu>
2024-04-14 20:28:32 -0700

    Refractor trade page to handle API errors

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -7,19 +7,24 @@ import { retrieveStockInfo } from '@/lib/actions';
 import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
+type APIStatusType = 'idle' | 'loading' | 'success' | 'error';
 
 const TradePage = () => {
   const searchParams = useSearchParams()
   const stockQuery = searchParams.get("stock") ?? 'AMZN'
   const [stockData, setStockData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [APIStatus, setAPIStatus] = useState<APIStatusType>('idle')
 
   useEffect(() => {
     const retrieveData = async () => {
-      setIsLoading(true)
+      setAPIStatus('loading')
       const stockDataArray = await retrieveStockInfo(stockQuery, "all")
       setStockData(stockDataArray)
-      setIsLoading(false)
+      if (!stockDataArray) {
+        setAPIStatus('error')
+      } else {
+        setAPIStatus('success')
+      }
     }
     retrieveData()
   }, [stockQuery])
@@ -28,19 +33,27 @@ const TradePage = () => {
     <main className="flex flex-col items-center mt-10">
       <Header stockSymbol={stockQuery} />
       <h1 className="mt-10">trade page</h1>
-      <h1>{`is loading: ${isLoading}`}</h1>
+      <h1>{`is loading: ${APIStatus}`}</h1>
       <div className="flex flex-col w-10/12">
         {
-          !isLoading ? (
+          APIStatus === 'error' ? (
+            <h1 className="text-2xl">there has been an API error</h1>
+          ) : null
+        }
+        {
+          APIStatus === 'loading' || APIStatus === 'idle' ? (
+            <h1 className="text-2xl">is loading please wait</h1>
+          ) : null
+        }
+        {
+          APIStatus === 'success' ? (
             <>
               <h1>{`Current price: ${stockData[0]}`}</h1>
               <InfoBoxList otherInfo={stockData[1]} />
               <ChartList chartData={stockData[2]} />
               <NewsList newsData={stockData[3]} />
             </>
-          ) : (
-            <h1 className="text-2xl">is loading please wait</h1>
-          )
+          ) : null
         }
       </div>
     </main>
