e3fde9b7d581877a924cc8df7468aaab60339205
ray-sjsu <raymund.mercader@sjsu.edu>
2024-04-15 16:28:18 -0700

    Temp workaround for navbar

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -6,14 +6,14 @@ import NavIcon from './NavIcon';
 import { getSession } from '@/lib/actions';
 
 const NavBar = async () => {
-  const session = await getSession()
+  // const session = await getSession()
 
   return (
-    <nav className="box-border w-full bg-red-700 fixed inset-x-0 bottom-0 h-20">
+    <nav className="box-border w-full bg-red-700 fixed inset-x-0 bottom-0 h-20 z-10">
       <div className="flex flex-row flex-nowrap items-center justify-around h-full w-full grow">
         <NavIcon href="" className="h-1/2" iconElement={<TbSearch className="size-full"/>} />
         <NavIcon href="/trade" className="h-4/6" iconElement={<TbDiamondFilled className="size-full"/>} />
-        <NavIcon href={ session.isLoggedIn ? "/profile" : "/login" } className="h-4/6" iconElement={<TbMenu2 className="size-full"/>} />
+        <NavIcon href={ false ? "/profile" : "/login" } className="h-4/6" iconElement={<TbMenu2 className="size-full"/>} />
       </div>
     </nav>
   )
