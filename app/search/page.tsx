2e62a1cd79b7dfa852e138f51574390fc03d7872
ray-sjsu <raymund.mercader@sjsu.edu>
2024-04-18 00:11:14 -0700

    Input validation search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,48 +1,45 @@
 'use client';
 import SearchMain from '@/components/search/SearchMain';
 import { retrieveStockSearchList } from '@/lib/actions';
-import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
 
 const SearchPage = () => {
     const [searchQuery, setSearchQuery] = useState("")
     const [searchResults, setSearchResults] = useState([])
-    const [submitted, setSubmitted] = useState(false)
-
-    const searchParams = useSearchParams()
-    const stock = searchParams.get("stock")
+    const [isSubmitting, setIsSubmitting] = useState(false)
 
     useEffect(() => {
         const retrieveSearchData = async () => {
             const stockSearchArray = await retrieveStockSearchList(searchQuery)
             setSearchResults(stockSearchArray)
-            setSubmitted(false)
+            setIsSubmitting(false)
         }
-        if (submitted) {
+        if (isSubmitting) {
             retrieveSearchData()
         }
-    }, [submitted])
+    }, [isSubmitting])
     
-
     const handleSearch = (query : string) => {
-        setSearchQuery(query)
+        const input = query.toUpperCase()
+        if (/^[A-Za-z]*$/.test(input)) {
+            setSearchQuery(input)
+        }
     }
-
     const handleOnClick = () => {
-        setSubmitted(true)
+        setIsSubmitting(true)
     }
 
     return (
         <main className="flex flex-col items-center">
             <section>
                 <h1>Search Page</h1>
-                <input type="text" onChange={e => handleSearch(e.target.value)} placeholder="Stock Symbol" className='text-red-500' />
+                <input type="text" onChange={e => handleSearch(e.target.value)} value={searchQuery} placeholder="Stock Symbol" className='text-red-500' maxLength={5} pattern="^[A-Za-z]*$" />
                 <button onClick={handleOnClick} className="p-2 bg-slate-600">Search</button>
             </section>
             <div className='flex flex-col gap-2'>
                 <p>{`search box search query: ${searchQuery}`}</p>
-                <p>{`is submitted: ${submitted}`}</p>
+                <p>{`is submitted: ${isSubmitting}`}</p>
             </div>
             <hr className="border w-full"></hr>
             <section className="flex flex-col gap-2">
