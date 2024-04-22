94d5676a1b3fadef00ee5d738127b9167ffeac1f
ray-sjsu <raymund.mercader@sjsu.edu>
2024-04-22 14:21:19 -0700

    Restructure types and add auth

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,8 +1,12 @@
 "use server";
 
+// api calls
+
 import { sessionOptions, SessionData, defaultSession } from "@/lib/lib"
 import { getIronSession } from "iron-session"
 import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
+import { stockAPIOptions } from "./types/types";
 
 export const getSession = async () => {
     const session = await getIronSession<SessionData>(cookies(), sessionOptions)
@@ -78,45 +82,3 @@ export const retrieveStockSearchList = async (stockSymbol: string) => {
         return null;
       }
 }
-
-
-// database actions
-
-import { PrismaClient } from "@prisma/client";
-import { TLoginFormSchema, stockAPIOptions } from "./types";
-import { redirect } from "next/navigation";
-const prisma = new PrismaClient()
-
-export async function retrieveLogin(LoginData : TLoginFormSchema) {
-    const users = prisma.userInfo.findUnique({
-        where: {
-            username: LoginData.username,
-            password: LoginData.password,
-        }
-    })
-    if (await users) {
-        return users
-    } else {
-        return null
-    }
-}
-
-
-export async function createTestUser() {
-    await prisma.userInfo.create({
-        data: {
-            userId: 1,
-            username: "test",
-            password: "test",
-        }
-    })
-    await prisma.userPreference.create({
-        data: {
-            userAccount: 1,
-            selectedStock: "APPL",
-            searchHistory: 'APPL, MSFT, AMZN',
-            favoriteList: 'APPL, AMZN',
-        }
-    })
-}
-
