18b86c4784420b66d0584ab60699ccdad4dbd12e
ray-sjsu <raymund.mercader@sjsu.edu>
2024-05-05 20:42:32 -0700

    Fix UI for search page with sign-in users. Some UI enhancements.

--- components/search/SearchPageUserPref.tsx
+++ components/search/SearchPageUserPref.tsx
@@ -1,14 +1,14 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
+"use client";
+import { getSession } from "next-auth/react";
 import React from "react";
 
 const SearchPageUserPref = async () => {
-  const session = await getServerSession(authOptions);
+  const session = await getSession();
   return (
     <>
       {session?.user ? (
         <section className="flex flex-col items-center mb-7">
-          <h1 className="text-red-500 text-2xl">{`You are logged in as ${session.user.username}`}</h1>
+          <h1 className="text-red-500 text-2xl font-semibold">{`You are logged in as ${session.user.username}`}</h1>
         </section>
       ) : null}
     </>
